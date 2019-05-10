9b42d554bad38680b0df4e6b0cb7209148bae73b
Xxcker <17624406021@163.com>
2019-05-10 21:37:24 +0800

    5.10
    跟踪记录显示

---
+++ src/com/ysd/service/NetFollowServiceImpl.java
@@ -0,0 +1,36 @@
+package com.ysd.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ysd.dao.NetFollowMapper;
+import com.ysd.entity.NetFollow;
+import com.ysd.entity.Pagination;
+@Service
+public class NetFollowServiceImpl implements NetFollowService {
+	@Autowired
+	private NetFollowMapper netMapper;
+	@Override
+	public Pagination selectNetFollow(Pagination pagin) {
+		List<NetFollow> selectNetFollow = netMapper.selectNetFollow(pagin);
+		Integer selectAllNetFollowCount = netMapper.selectAllNetFollowCount(pagin);
+		pagin.setRows(selectNetFollow);
+		pagin.setTotal(selectAllNetFollowCount);
+		return pagin;
+	}
+
+	@Override
+	public Integer selectAllNetFollowCount(Pagination pagin) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer insertNetFollow(NetFollow netfollow) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
